eb5d473646f9feba7c913d04197d4f4b72f11879
Lisa Baerga <msbaerga@gmail.com>
2020-06-22 19:02:01 -0400

    forgot percentage of day

--- pre02/Time.java
+++ pre02/Time.java
@@ -3,14 +3,18 @@ public class Time {
     int hour = 6;
     int minute = 06;
     int second = 00;
-    int secondCountMin = minute * 60;
     int secondCountHour = hour * 60 * 60;
+    int secondCountMin = minute * 60;
+    int secondCountSec = second * 60;
     int secondInDay = 86400;
     int timeSinceMid = secondCountMin + secondCountHour;
     int timeToMid = secondInDay - timeSinceMid;
     int newHour = 6;
     int newMinute = 25;
     int newSecond = 00;
+    double minutesOfDay = 24 * 60;
+    double minutesPassed = newHour * 60 + newMinute;
+    double percentOfDay = minutesPassed*100/minutesOfDay;
     System.out.println("Seconds since midnight:");
     System.out.println(timeSinceMid);
     System.out.println("Seconds to midnight:");
@@ -23,5 +27,7 @@ public class Time {
     System.out.print(newSecond-second);
     System.out.print(" seconds, ");
     System.out.println("all well spent!");
+    System.out.println(minutesPassed);
+    System.out.println(percentOfDay);
   }
 }
