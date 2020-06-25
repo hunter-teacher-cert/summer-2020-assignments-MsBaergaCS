c090c861e0a45432e09f778584d3e86bbbbe74b0
Lisa Baerga <msbaerga@gmail.com>
2020-06-25 11:11:41 -0400

    add isTriangle method

--- pre05/Methods.java
+++ pre05/Methods.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.lang.*;
+import java.util.Scanner;
 
 public class Methods {
 
@@ -8,9 +9,20 @@ public class Methods {
       System.out.println("Those numbers are divisible!");
       return true;
     } else {
-      System.out.println("Those numbers are divisible!");
+      System.out.println("Those numbers are NOT divisible!");
       return false;
     }
+ }
+
+ public static boolean isTriangle (int a, int b, int c) {
+   if (a + b < c || b + c < a || a + c < b) {
+     System.out.println("Sorry - it's NOT a triangle!");
+     return false;
+   } else {
+     System.out.println("Congratulations - it's a triangle!");
+     return true;
+   }
+
  }
   public static void main(String[] args) {
     int numberDivFirst;
@@ -25,5 +37,22 @@ public class Methods {
 
     isDivis (numberDivFirst, numberDivSecond);
 
+    int triNumOne;
+    int triNumTwo;
+    int triNumThree;
+    Scanner triInput = new Scanner(System.in);
+    Scanner triInput2 = new Scanner(System.in);
+    Scanner triInput3 = new Scanner(System.in);
+    System.out.println("Let's check if three numbers can make a triangle!");
+    System.out.print("Enter the first number:");
+    triNumOne = triInput.nextInt();
+    System.out.print("Enter the second number:");
+    triNumTwo = triInput2.nextInt();
+    System.out.print("Enter the third number:");
+    triNumThree = triInput3.nextInt();
+
+    isTriangle (triNumOne, triNumTwo, triNumThree);
     }
+
+
 }
