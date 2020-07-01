70d45414948cc34a61d2109b0fc6329ab2c8b0fe
Lisa Baerga <msbaerga@gmail.com>
2020-07-01 19:58:22 -0400

    finished diagonal

--- classwork/dayNumOne.java
+++ classwork/dayNumOne.java
@@ -5,46 +5,46 @@ public class dayNumOne {
 
   public static void main(String[] args) {
 
-// creates 20 rows
-//creates 20 columns
+    // creates 20 rows
+    //creates 20 columns
     int rows = 20;
     int cols = 20;
 
-//creates a new 2d arrray called grids, which is made up of 20r 20c
+    //creates a new 2d arrray called grids, which is made up of 20r 20c
     int[][] arr = new int[rows][cols];
 
-//calls method to print 2d array
+    //calls method to print 2d array
     //print2d(arr);
-//calls rowPop method
+    //calls rowPop method
 
     //rowPop(arr, 2, 10);
 
-//calls colPop method
+    //calls colPop method
     colPop(arr, 3, 255);
     print2d(arr);
-//calls invert method to invert 0s and 250s
+    //calls invert method to invert 0s and 250s
 
     invert(arr);
     print2d(arr);
     System.out.println();
 
-    diagonal(arr, 1, 3, 3, 1);
+    diagonal(arr, 19, 19, 0, 1);
 
     print2d(arr);
   } //end main
 
 
-//prints 2d array
+  //prints 2d array
   public static void print2d(int[][] a) {
 
-//iterates through row
+    //iterates through row
     for (int r=0; r < a.length; r++) {
-//finishing each column across before moving to next row
+      //finishing each column across before moving to next row
       for (int c=0; c < a[r].length; c++) {
 
-      System.out.printf("%03d ", a[r][c]);
+        System.out.printf("%03d ", a[r][c]);
       }//end column for loop
-//prints line after each row of columns
+      //prints line after each row of columns
       System.out.println();
     }//end row for loop
 
@@ -76,31 +76,34 @@ public class dayNumOne {
   }//end invert
 
   /*
-    0: up + left
-    1: up + right
-    2: down + left
-    3: down + right
-   */
+  0: up + left
+  1: up + right
+  2: down + left
+  3: down + right
+  */
 
-  // not woking
+  // not working needs revision
   public static void diagonal(int[][] a, int r, int c, int direction, int value) {
     while ((c >= 0) && (r >=0) && (r < a.length) && (c < a[r].length)){
-      a[r][c] = value;
       if (direction == 0) { //dirr up/left
+        a[r][c] = value;
         r = r - 1;
         c = c - 1;
       }
       if (direction == 1){ //dir up/right
-        r = r + 1;
+        a[r][c] = value;
+        r = r - 1;
         c = c + 1;
       }
       if (direction == 2){ //dir down/left
-        r = r - 1;
-        c = c - 1;
+        a[r][c] = value;
+        r = r + 1;
+        c = c + 1;
       }
       if (direction == 3){ //dir down/right
-        r = r - 1;
-        c = c + 1;
+        a[r][c] = value;
+        r = r + 1;
+        c = c - 1;
       }
     } //end while loop
   }//end diagonal
