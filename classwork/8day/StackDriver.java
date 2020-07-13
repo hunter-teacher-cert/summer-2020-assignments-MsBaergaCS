f008f4464dbbe2e5adac4a2cc3ef626c9fc37da8
Lisa Baerga <msbaerga@gmail.com>
2020-07-13 15:28:33 -0400

    commented better StackDriver

--- classwork/8day/StackDriver.java
+++ classwork/8day/StackDriver.java
@@ -1,6 +1,6 @@
 public class StackDriver{
 
-  public static String reverse(String s){
+  public static String reverse(String s){   //comments in MyOtherStack.java
     MyOtherStack rev =  new MyOtherStack();
     String printRev = "";
     for (int i = 0; i< s.length(); i++){
@@ -12,7 +12,7 @@ public class StackDriver{
       return printRev;
   }
 
-  public static boolean isPalendrome(String s){
+  public static boolean isPalendrome(String s){  //comments in MyOtherStack.java
     return s.equals(reverse(s));
     }
 
@@ -25,11 +25,12 @@ public class StackDriver{
     } else {
       System.out.println("is not empty");
     }
-
+//push a known palindrome
     thisStack.push("a");
     thisStack.push("n");
     thisStack.push("n");
     thisStack.push("a");
+//check size
     System.out.print(thisStack.stackSize());
 
     //System.out.println(thisStack.stackSize());
@@ -41,12 +42,16 @@ public class StackDriver{
 
     //System.out.println(isPalendrome(test));
 
+//set string to anna, the known palindrome
     String anna = new String ("anna");
+
+  //check if palindrome
     System.out.println(isPalendrome(anna));
+    // c heck if palindrome
     if(isPalendrome(anna)==true){
-      System.out.println(anna + "it is a Palendrome");
+      System.out.println(anna + " it is a Palendrome");
     } else {
-      System.out.println(anna + "is not a Palendrome");
+      System.out.println(anna + " is not a Palendrome");
     }
 /*
   MyStack thisStack =  new MyStack();
