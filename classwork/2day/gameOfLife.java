853635eaa477e4b7891caed9f145717eb2f2eaa9
Lisa Baerga <msbaerga@gmail.com>
2020-07-30 22:06:35 -0400

    commented GOL

--- classwork/2day/gameOfLife.java
+++ classwork/2day/gameOfLife.java
@@ -16,37 +16,37 @@ apply GOL rules
 
 //**FUNCTIONAL**
 public static char [][] createNewBoard (int rows, int cols) {
-  char[][] board = new char [rows][cols];
-  for (int r = 0; r < rows; r++){
-    for (int c = 0; c < cols; c++){
-      int sq = (int)(Math.random()*10 + 1);
-      if (sq < 2){
-        board[r][c] = ' ';
+  char[][] board = new char [rows][cols];   //gens new board
+  for (int r = 0; r < rows; r++){         //follows rows
+    for (int c = 0; c < cols; c++){         //follows columns
+      int sq = (int)(Math.random()*10 + 1);   //for each sq generates a random value
+      if (sq < 2){                          //if it is less than 2, i.e. about 1/10 chance
+        board[r][c] = ' ';                //it prints a blank position
       } else{
-        board[r][c] = 'X';
+        board[r][c] = 'X';                //otherwise it prints an x
       } //end else condition
     } //end for Loops
   } //end outer for loop
-  return board;
+  return board;                       //returns entire double array
 }//end method
 
 //**FUNCTIONAL**
 public static char [][] createBlankBoard (int rows, int cols) {
-  char[][] board = new char [rows][cols];
-  for (int r = 0; r < rows; r++){
-    for (int c = 0; c < cols; c++){
-        board[r][c] = ' ';
+  char[][] board = new char [rows][cols]; //gens blank board
+  for (int r = 0; r < rows; r++){     //follows rows
+    for (int c = 0; c < cols; c++){     //follows columns
+        board[r][c] = ' ';          //adds a space to that position
     } //end for Loops
   } //end outer for loop
-  return board;
+  return board;             //returns double array holding blank board data
 }//end method
 
 
 //**FUNCTIONAL**
   public static void printBoard (char[][] board){
-    for (int r = 0; r < board.length; r++){
-      for (int c = 0; c <board[r].length; c++){
-        System.out.printf("%c", board[r][c]);
+    for (int r = 0; r < board.length; r++){  //iterates thru board follows rows
+      for (int c = 0; c <board[r].length; c++){  //follows columns
+        System.out.printf("%c", board[r][c]); //prints the coordinates
       }
       System.out.println();
     }
@@ -55,8 +55,8 @@ public static char [][] createBlankBoard (int rows, int cols) {
 
   //**FUNCTIONAL**
   public static void setCell(char[][] board, int r, int c, char value){
-    if (r>=0 && r < board.length && c >= 0 && c<board[r].length){
-      board[r][c] = value;
+    if (r>=0 && r < board.length && c >= 0 && c<board[r].length){ //checks OOB
+      board[r][c] = value;  //adds input value to the position given
     }
   }
   //count neighbors living around printBoard
@@ -67,52 +67,52 @@ public static char [][] createBlankBoard (int rows, int cols) {
     int rowNum = board.length;
     int colNum = board[0].length;
 
-      for (int i = r-1; i <= r + 1; i++){
-        for (int j = c-1; j <= c + 1; j++){
-          if (i >= 0 && i < rowNum && j >= 0 && j < colNum) {
-            if (i != r || j != c) {
-              if (board[i][j] == 'X') {
-              count++;
+      for (int i = r-1; i <= r + 1; i++){  //outside loops checks rows
+        for (int j = c-1; j <= c + 1; j++){   //inside loop checks columns
+          if (i >= 0 && i < rowNum && j >= 0 && j < colNum) { //checks to see if it is in bounds
+            if (i != r || j != c) {  //makes sure we dont count the square itself
+              if (board[i][j] == 'X') {  //checks if it is present
+              count++;                //adds to living count
           }
         }
       }
     }
     }
-    return count;
+    return count;  //gives total living neighbor count
   }
   //given a board and a cell, look at neighbors and determine status
   //based on Conways GOL rules
   //determine if alive ('X') or dead ('')
 
   public static char getNextGenCell (char[][] board, int r, int c){
-    char currentSq = board[r][c];
+    char currentSq = board[r][c];       //sets individual square based on count neighbo method below, uses that to determine actions w series of if statements
     int count = countNeighbors(board, r, c);
-    if ((currentSq =='X') && (count > 3) || (count < 2)) {
-      return ' ';
+    if ((currentSq =='X') && (count > 3) || (count < 2)) { //checks to see which conditions of
+      return ' ';                                           //GOL rules are met, must have an x and either a count of more than 3 o less than 2
+    }                                                       //kills or generates
+    else if ((currentSq=='X')&&((count == 3) || (count == 2))){ //or maintains based on that data
+      return 'X';                                           //checks to see if it is x and either equal to 2 o 3.
     }
-    else if ((currentSq=='X')&&((count == 3) || (count == 2))){
-      return 'X';
-    }
-    else if ((currentSq==' ')&&(count== 3)){
-      return 'X';
-    } else {
-      return ' ';
+    else if ((currentSq==' ')&&(count== 3)){    // if neither above condition is true it checks to see if
+      return 'X';                               //it is blank (dead) and the neighbors count is 3
+    } else {                                    //if so it generates a birth
+      return ' ';                                 //otherwise no conditions met -- square is blank.
     }
   }
 
 //
   public static char[][] generateNextBoard(char[][] board){
-    char newBoard[][] = new char[25][25];
-    for (int r = 0; r < 25; r++){
+    char newBoard[][] = new char[25][25];     //using getNextGenCell, which builds off countNeighbors
+    for (int r = 0; r < 25; r++){               //iterates thru a board and determines the value of each cell
       for (int c = 0; c < 25; c++){
-          newBoard[r][c] = getNextGenCell(board, r, c);
+          newBoard[r][c] = getNextGenCell(board, r, c); //saving each square as the value returned based off of the state of play on the previous game board
       } //end for Loops
     } //end outer for loop
     // fill the new board
-    return newBoard;
+    return newBoard;      //returns entire double array
    }
 //not able to call
-  public static void animateBoard(char[][] board){
+  public static void animateBoard(char[][] board){  //animates series of boards to allow visual of the game over a seies of turns
 
     //send ANSI control char to clear terminal and put cursor top left
       System.out.print("[0;0H\n");
