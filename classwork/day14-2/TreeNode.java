0be35b80825d0185181496c533b2eecc4ed213f6
Lisa Baerga <msbaerga@gmail.com>
2020-07-31 22:19:32 -0400

    commit treenode & comments

---
+++ classwork/day14-2/TreeNode.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class TreeNode{
+    private int data;
+    private TreeNode left,right;
+  //  private int count; could track # of items that are a given value
+
+    public TreeNode(){
+	     this(0);
+    }
+//defining left and right of root as Null
+//taking in data for root
+    public TreeNode(int data){
+	     this.data = data;
+	     left = null;
+	     right = null;
+    }
+
+//setters
+    public void setData(int data){
+	this.data = data;
+    }
+
+    public void setLeft(TreeNode left){
+	this.left = left;
+    }
+
+    public void setRight(TreeNode right){
+	this.right = right;
+    }
+
+
+//getters
+    public int getData(){
+	return data;
+    }
+
+    public TreeNode getLeft(){
+	return left;
+    }
+
+    public TreeNode getRight(){
+	return right;
+    }
+}
